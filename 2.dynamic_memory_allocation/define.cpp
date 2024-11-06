3a0e83c6f99fbc3f3b45cefb5ad5d98ec22daf44
Dhruvrajsinh Zala <dnzala2414@gmail.com>
2024-11-07 00:00:51 +0530

    global vars

--- 2.dynamic_memory_allocation/define.cpp
+++ 2.dynamic_memory_allocation/define.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
 #define PI 3.14
+#define SQUARE(x) (x * x) // macro
 
 int main()
 {
-    int r;
+    // int r;
 
-    cin >> r;
-    cout << PI * r * r << endl;
+    // cin >> r;
+    // cout << PI * r * r << endl;
+
+    int x = 36 / SQUARE(6);
+
+    cout << x;
+
+    return 0;
 }
--- 2.dynamic_memory_allocation/global_variable.cpp
+++ 2.dynamic_memory_allocation/global_variable.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int i;
+// int i;
 
-void g()
+// void g()
+// {
+//     cout << i << endl;
+//     i++;
+// }
+// void f()
+// {
+//     cout << i << endl;
+//     i++;
+//     g();
+// }
+
+int x = 1;
+
+void print()
 {
-    cout << i << endl;
-    i++;
-}
-void f()
-{
-    cout << i << endl;
-    i++;
-    g();
+    int x = 2;
+    {
+        int x = 3;
+        cout << x << endl;
+    }
 }
 
 int main()
 {
-    i = 69;
-    f();
-    cout << i << endl;
+    // i = 69;
+    // f();
+    // cout << i << endl;
+
+    print();
+    return 0;
 }
