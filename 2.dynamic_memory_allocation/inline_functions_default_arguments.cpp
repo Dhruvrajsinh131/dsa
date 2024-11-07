2407c1cf7f37daa851c28b3ea0939c9f304cae97
Dhruvrajsinh Zala <dnzala2414@gmail.com>
2024-11-07 23:45:39 +0530

    fix: inline functions & arguments

---
+++ 2.dynamic_memory_allocation/inline_functions_default_arguments.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int max(int a, int b)
+{
+
+    return (a > b) ? a : b;
+}
+
+int sum(int *arr, int size, int start = 0)
+{
+    int sum = 0;
+
+    for (int i = start; i < size; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int sum2(int a, int b = 0, int c = 0, int d = 0)
+{
+
+    return a + b + c + d;
+}
+
+int main()
+{
+    int a = 23, b = 45;
+
+    int c = max(a, b);
+
+    cout << "max = " << c << endl;
+    int x = 546, y = 87;
+
+    int z = max(x, y);
+    cout << "max = " << z << endl;
+
+    int arr[] = {1, 2, 3};
+    int size = 3;
+
+    cout << sum(arr, 3, 1) << endl;
+
+    cout << sum2(1) << endl;
+    cout << sum2(1, 2) << endl;
+    cout << sum2(1, 2, 3) << endl;
+    cout << sum2(1, 2, 3, 4) << endl;
+}
