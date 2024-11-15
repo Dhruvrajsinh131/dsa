0c64d1010e62eec0bc46f4df6c9196d4901068cf
Dhruvrajsinh Zala <dnzala2414@gmail.com>
2024-11-15 23:28:41 +0530

    string to int

--- 4.recursion_assignment_2/string_to_integer.cpp
+++ 4.recursion_assignment_2/string_to_integer.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int convertToInt(char *str, int index)
+// int convertToInt(char *str, int start, int end)
+
+// {
+
+//     if (start > end)
+//     {
+//         return 0;
+//     }
+
+//     int num = str[end] - '0';
+
+//     return num + convertToInt(str, start, end - 1) * 10;
+// }
+
+int convertToInt2(char *str, int i, int size)
+
 {
-    if (str[index] == '\0')
+
+    if (size == 0)
     {
         return 0;
     }
 
-    int result = convertToInt(str, index + 1);
-    int number = str[index] - '0';
+    int num = str[i] - '0';
 
-    return number * 10 + result;
+    int getPower = 1;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        getPower *= 10;
+    }
+
+    return num * getPower + convertToInt2(str, i + 1, size - 1);
 }
 
 int main()
 {
     char str[] = "345";
 
-    int result = convertToInt(str, 0);
+    // int result = convertToInt(str, 0, 2); // (string ,start , end)
+    int result = convertToInt2(str, 0, 3); // (string ,index , length)
 
     cout << result;
 }
