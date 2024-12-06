c6dc6b68049aa3c751e74a45dde85706453b04d1
Dhruvrajsinh Zala <dnzala2414@gmail.com>
2024-12-06 15:48:09 +0530

    print keypad combos

--- 5.recursion_2/keypad.cpp
+++ 5.recursion_2/keypad.cpp
@@ -68,4 +68,6 @@ int main()
     {
         cout << op[i] << endl;
     }
+
+    delete[] op;
 }
---
+++ 5.recursion_2/print_keypad_combos.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+string keypad[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+void printCombos(int input, string output)
+{
+    if (input == 0)
+    {
+        cout << output << endl;
+        return;
+    }
+
+    int digit = input % 10;
+    string letters = keypad[digit];
+
+    for (size_t i = 0; i < letters.size(); i++)
+    {
+        printCombos(input / 10, letters[i] + output);
+    }
+}
+
+int main()
+{
+    int input;
+
+    cout << "Enter the input : ";
+    cin >> input;
+
+    printCombos(input, "");
+}
