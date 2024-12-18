7251531e822174b93f5da65939cd59756d5e64b7
Dhruvrajsinh Zala <67993600+Dhruvrajsinh131@users.noreply.github.com>
2024-12-18 23:56:00 +0530

    OOPS stared

---
+++ 9.OOPS1/product.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+class Prouct
+{
+    int id;
+    string name;
+    int price;
+};
---
+++ 9.OOPS1/student.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+    string name;
+    int std;
+    int age;
+};
