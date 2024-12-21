f6c005e4a1e4a363e0f061b30e426e59b4a0a66b
Dhruvrajsinh Zala <67993600+Dhruvrajsinh131@users.noreply.github.com>
2024-12-22 00:02:43 +0530

    constructor

--- 9.OOPS1/contructor.cpp
+++ 9.OOPS1/contructor.cpp
@@ -4,27 +4,66 @@ using namespace std;
 class Student
 {
 public:
+    int rollNumber;
+
+private:
     int age;
-    Student()
+
+public:
+    // Student()
+    // {
+    //     cout << "Constructor called" << endl;
+    // }
+
+    Student(int rollNumber)
     {
-        cout << "Contructor Called" << endl;
+        this->rollNumber = rollNumber;
     }
 
-    Student(int n)
+    Student(int r, int a)
     {
-        cout << "Constructor 2 Called" << endl;
-        // this holds the adddress of current Obj
-        cout << this << endl;
-        this->age = n;
+        rollNumber = r;
+        age = a;
+        cout << "this = " << this << endl;
+    }
+    void display()
+    {
+        cout << rollNumber << " " << age << endl;
+    }
+
+    int getAge()
+    {
+        return age;
     }
 };
 
 int main()
 {
-    Student s1;
 
-    s1.age = 70;
+    //     Student *s1 = new Student(10);
 
-    Student s2(20);
-    cout << s2.age << " " << &s2;
+    //     s1->display();
+
+    //     Student s2(21, 20);
+
+    //     s2.display();
+    // Student *s3 = new Student();
+
+    // s3->display();
+
+    // cout << "addressOf s1 = " << &s1 << endl;
+    // cout << "s1 rollNO = " << s1->rollNumber << endl;
+    // Student *s2 = new Student(*s1);
+
+    // cout << "s2 rollNO = " << s2->rollNumber << endl;
+
+    Student *s1 = new Student(10, 21);
+
+    s1->display();
+    Student *s2 = new Student(11, 23);
+    s2->display();
+
+    s2 = s1;
+
+    s2->display();
 }
---
+++ 9.OOPS1/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+ endl;
+    Student *s2 = new Student(*s1);
+
+    cout << "s2 rollNO = " << s2->rollNumber << endl;
