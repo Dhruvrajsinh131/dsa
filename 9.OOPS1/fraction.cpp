c77c17c13e41e665fdf9f5eaa83c323a8f7ffd9c
Dhruvrajsinh Zala <67993600+Dhruvrajsinh131@users.noreply.github.com>
2024-12-22 01:16:21 +0530

    fraction problem

---
+++ 9.OOPS1/fraction.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+class Fraction
+{
+private:
+    int numarator;
+    int denominator;
+
+public:
+    Fraction(int n, int d)
+    {
+        this->numarator = n;
+        this->denominator = d;
+    }
+
+    void print()
+    {
+        cout << this->numarator << "/" << this->denominator << endl;
+    }
+
+    void simplify()
+    {
+        int gcd = 1;
+        int j = min(this->numarator, this->denominator);
+        for (int i = 1; i <= j; i++)
+        {
+            if (this->numarator % i == 0 & this->denominator % i == 0)
+            {
+                gcd = i;
+            }
+        }
+        this->numarator = this->numarator / gcd;
+        this->denominator = this->denominator / gcd;
+    }
+
+    void add(Fraction f2)
+    {
+
+        int lcm = this->denominator * f2.denominator;
+        int upperValue = (this->numarator * f2.denominator) + (this->denominator * f2.numarator);
+
+        this->denominator = lcm;
+        this->numarator = upperValue;
+        simplify();
+    }
+};
+
+int main()
+{
+    Fraction *f1 = new Fraction(10, 3);
+    f1->print();
+
+    Fraction *f2 = new Fraction(10, 3);
+    f1->add(*f2);
+
+    f1->print();
+
+    delete f1;
+    delete f2;
+}
