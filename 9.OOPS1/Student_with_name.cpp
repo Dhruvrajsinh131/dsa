8bb7729ba4fc98f6a0de40de057891d1f6c0ac02
Dhruvrajsinh Zala <67993600+Dhruvrajsinh131@users.noreply.github.com>
2024-12-24 23:56:09 +0530

    copy constructor

--- 9.OOPS1/Student_with_name.cpp
+++ 9.OOPS1/Student_with_name.cpp
@@ -5,9 +5,9 @@ class Student_with_name
 {
 
     int age;
-    char *name;
 
 public:
+    char *name;
     Student_with_name(int age, char *name)
     {
         this->age = age;
@@ -17,6 +17,16 @@ public:
         // Deep Copy
         this->name = new char[strlen(name) + 1];
         strcpy(this->name, name);
+    }
+
+    // Copy Contstructor
+    Student_with_name(Student_with_name const &s)
+    {
+
+        this->age = s.age;
+        // this->name = s.name; // Shallow Copy X
+        this->name = new char[strlen(s.name) + 1];
+        strcpy(this->name, s.name);
     }
 
     void display()
--- 9.OOPS1/Student_with_name_Use.cpp
+++ 9.OOPS1/Student_with_name_Use.cpp
@@ -7,16 +7,24 @@ int main()
 
     char name[] = "Dhruvraj";
     Student_with_name *s1 = new Student_with_name(23, name);
+
     // Student_with_name s1(23, name);
 
     // s1.display();
+
+    Student_with_name *s2 = new Student_with_name(*s1);
+    s2->name[0] = 'V';
+    s1->display();
+    s2->display();
     s1->display();
 
-    name[3] = 'v';
-    Student_with_name *s2 = new Student_with_name(22, name);
+    // name[3] = 'v';
+    // Student_with_name *s2 = new Student_with_name(22, name);
     // Student_with_name s2(22, name);
 
     // s2.display();
     // s1.display();
-    s1->display();
+    // s1->display();
+
+    Student_with_name *s3 = new Student_with_name(23, name);
 }
