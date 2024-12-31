72d2094070f7edc77f28ba77382725612198dad9
Dhruvrajsinh Zala <67993600+Dhruvrajsinh131@users.noreply.github.com>
2024-12-31 23:43:02 +0530

    Linked List Take input

--- 11.LinkedList/LinkedList.cpp
+++ 11.LinkedList/LinkedList.cpp
@@ -2,6 +2,36 @@
 using namespace std;
 #include "Node.cpp"
 
+Node *takeInput()
+{
+    int data;
+    cin >> data;
+
+    Node *head = nullptr;
+
+    while (data != -1)
+    {
+        Node *n = new Node(data);
+        if (head == nullptr)
+        {
+            head = n;
+        }
+        else
+        {
+            Node *temp = head;
+
+            while (temp->next != nullptr)
+            {
+                temp = temp->next;
+            }
+            temp->next = n;
+        }
+
+        cin >> data;
+    }
+    return head;
+}
+
 void print(Node *head)
 {
 
@@ -19,19 +49,21 @@ int main()
 {
 
     // Statically
-    Node n1(2);
-    Node n2(4);
-    Node n3(6);
-    Node n4(8);
-    Node n5(10);
+    // Node n1(2);
+    // Node n2(4);
+    // Node n3(6);
+    // Node n4(8);
+    // Node n5(10);
 
-    // Defining Head of Liked List
-    Node *head = &n1;
+    // // Defining Head of Liked List
+    // Node *head = &n1;
 
-    n1.next = &n2;
-    n2.next = &n3;
-    n3.next = &n4;
-    n4.next = &n5;
+    // n1.next = &n2;
+    // n2.next = &n3;
+    // n3.next = &n4;
+    // n4.next = &n5;
+
+    Node *head = takeInput();
 
     print(head);
 
