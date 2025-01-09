b45bdfb9c3e2d27b7a8af78fa9409d37f7db49a7
Dhruvrajsinh Zala <67993600+Dhruvrajsinh131@users.noreply.github.com>
2025-01-09 23:48:46 +0530

    print reverse LL

---
+++ 11.LinkedList/assignments/print_reverse_LL.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+#include "Node.cpp"
+
+Node *takeInput()
+{
+    int data;
+
+    Node *head = nullptr;
+    Node *tail = nullptr;
+
+    cin >> data;
+
+    while (data != -1)
+    {
+        Node *newNode = new Node(data);
+
+        if (head == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = tail->next;
+        }
+
+        cin >> data;
+    }
+
+    return head;
+}
+
+void print_LL(Node *head)
+{
+    if (head == nullptr)
+    {
+        return;
+    }
+
+    cout << head->data << " ";
+
+    print_LL(head->next);
+}
+
+void rev_print_LL(Node *head)
+{
+    if (head->next == nullptr)
+    {
+        cout << head->data << " ";
+        return;
+    }
+    rev_print_LL(head->next);
+    cout << head->data << " ";
+}
+
+int main()
+{
+    Node *head = takeInput();
+
+    cout << endl;
+    print_LL(head);
+    cout << endl;
+    rev_print_LL(head);
+}
