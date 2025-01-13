48f14f5c4265e6183de3fb66b37aaf727048523a
Dhruvrajsinh Zala <67993600+Dhruvrajsinh131@users.noreply.github.com>
2025-01-13 23:51:01 +0530

    LL mid point

---
+++ 12.LinkedList2/findMidPointOfLL.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+#include "Node.cpp"
+
+Node *takeInput()
+{
+    int data;
+
+    Node *head = nullptr;
+    Node *tail = nullptr;
+
+    cin >> data;
+
+    while (data != -1)
+    {
+        Node *newNode = new Node(data);
+
+        if (head == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+
+    return head;
+}
+
+void print_LL(Node *head)
+{
+
+    Node *temp = head;
+
+    while (temp != nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int getMidPoint(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow->data;
+}
+
+int main()
+{
+
+    Node *head = takeInput();
+    print_LL(head);
+
+    cout << "Mid element is = " << getMidPoint(head);
+}
