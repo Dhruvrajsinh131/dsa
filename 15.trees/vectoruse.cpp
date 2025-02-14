f17ff07ebbde570afb82e4c0a8c70bfadb7974c3
Dhruvrajsinh Zala <67993600+Dhruvrajsinh131@users.noreply.github.com>
2025-02-15 00:14:48 +0530

    vectors

--- 15.trees/vectoruse.cpp
+++ 15.trees/vectoruse.cpp
@@ -6,11 +6,40 @@ int main()
 {
     vector<int> v;
     // vector<int> *vp = new vector<int>();
-    v.push_back(10);
-    v.push_back(20);
-    v.push_back(30);
+    // v.push_back(10);
+    // v.push_back(20);
+    // v.push_back(30);
 
-    cout << v[0] << endl;
-    cout << v[1] << endl;
-    cout << v[2] << endl;
+    // v[0] = 2000;
+
+    // v[3] = 24324;
+
+    // v.push_back(324);
+    // v[6] = 45;
+
+    // cout << v[0] << endl;
+    // cout << v[1] << endl;
+    // cout << v[2] << endl;
+    // cout << v[3] << endl;
+    // cout << v[4] << endl;
+    // cout << v[5] << endl;
+    // cout << v[6] << endl;
+
+    // v.pop_back();
+
+    // cout << "Size = " << v.size() << endl;
+
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //     cout << v[i] << " ";
+    // }
+
+    for (int i = 0; i < 100; i++)
+    {
+        v.push_back(i + 1);
+
+        cout << v.size() << endl;
+    }
+
+    cout << v.capacity();
 }
