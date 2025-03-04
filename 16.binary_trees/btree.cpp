7fe96e1abfa49bed4d0ca1b3c4c4fb9e652beea8
Dhruvrajsinh Zala <67993600+Dhruvrajsinh131@users.noreply.github.com>
2025-03-04 22:25:49 +0530

    binary trees

---
+++ 16.binary_trees/btree.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+using namespace std;
+
+// Definition of a Node
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int value)
+    {
+        data = value;
+        left = right = nullptr;
+    }
+};
+
+// Binary Tree Class
+class BinaryTree
+{
+public:
+    Node *root;
+
+    BinaryTree()
+    {
+        root = nullptr;
+    }
+
+    // Insert a node
+    Node *insert(Node *root, int data)
+    {
+        if (root == nullptr)
+        {
+            return new Node(data);
+        }
+        if (data < root->data)
+        {
+            root->left = insert(root->left, data);
+        }
+        else
+        {
+            root->right = insert(root->right, data);
+        }
+        return root;
+    }
+
+    // Inorder Traversal (Left, Root, Right)
+    void inorder(Node *root)
+    {
+        if (root != nullptr)
+        {
+            inorder(root->left);
+            cout << root->data << " ";
+            inorder(root->right);
+        }
+    }
+
+    // Preorder Traversal (Root, Left, Right)
+    void preorder(Node *root)
+    {
+        if (root != nullptr)
+        {
+            cout << root->data << " ";
+            preorder(root->left);
+            preorder(root->right);
+        }
+    }
+
+    // Postorder Traversal (Left, Right, Root)
+    void postorder(Node *root)
+    {
+        if (root != nullptr)
+        {
+            postorder(root->left);
+            postorder(root->right);
+            cout << root->data << " ";
+        }
+    }
+
+    // Find the minimum value node (used in deletion)
+    Node *findMin(Node *root)
+    {
+        while (root->left != nullptr)
+        {
+            root = root->left;
+        }
+        return root;
+    }
+
+    // Delete a node
+    Node *deleteNode(Node *root, int key)
+    {
+        if (root == nullptr)
+            return root;
+
+        if (key < root->data)
+        {
+            root->left = deleteNode(root->left, key);
+        }
+        else if (key > root->data)
+        {
+            root->right = deleteNode(root->right, key);
+        }
+        else
+        {
+            // Case 1: No child
+            if (root->left == nullptr && root->right == nullptr)
+            {
+                delete root;
+                return nullptr;
+            }
+            // Case 2: One child
+            else if (root->left == nullptr)
+            {
+                Node *temp = root->right;
+                delete root;
+                return temp;
+            }
+            else if (root->right == nullptr)
+            {
+                Node *temp = root->left;
+                delete root;
+                return temp;
+            }
+            // Case 3: Two children
+            Node *temp = findMin(root->right);
+            root->data = temp->data;
+            root->right = deleteNode(root->right, temp->data);
+        }
+        return root;
+    }
+};
+
+int main()
+{
+    BinaryTree tree;
+    tree.root = tree.insert(tree.root, 50);
+    tree.insert(tree.root, 30);
+    tree.insert(tree.root, 70);
+    tree.insert(tree.root, 20);
+    tree.insert(tree.root, 40);
+    tree.insert(tree.root, 60);
+    tree.insert(tree.root, 80);
+
+    cout << "Inorder Traversal: ";
+    tree.inorder(tree.root);
+    cout << "\n";
+
+    cout << "Preorder Traversal: ";
+    tree.preorder(tree.root);
+    cout << "\n";
+
+    cout << "Postorder Traversal: ";
+    tree.postorder(tree.root);
+    cout << "\n";
+
+    tree.root = tree.deleteNode(tree.root, 30);
+    cout << "After Deleting 30, Inorder Traversal: ";
+    tree.inorder(tree.root);
+    cout << "\n";
+
+    return 0;
+}
